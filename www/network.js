6e6c15c0be885871b9e205943bbde5897e4bafc9
KUDO Ken-ichi <kudo.k@wingarc.com>
2024-06-12 10:22:11 +0900

    Androidでのみ機能するよう修正

--- www/network.js
+++ www/network.js
@@ -23,69 +23,79 @@ var cordova = require('cordova');
 var channel = require('cordova/channel');
 var utils = require('cordova/utils');
 
-// Link the onLine property with the Cordova-supplied network info.
-// This works because we clobber the navigator object with our own
-// object in bootstrap.js.
-// Browser platform do not need to define this property, because
-// it is already supported by modern browsers
-if (cordova.platformId !== 'browser' && typeof navigator !== 'undefined') {
-    utils.defineGetter(navigator, 'onLine', function () {
-        return this.connection.type !== 'none';
-    });
-}
-
-function NetworkConnection () {
-    this.type = 'unknown';
-}
-
 /**
- * Get connection info
- *
- * @param {Function} successCallback The function to call when the Connection data is available
- * @param {Function} errorCallback The function to call when there is an error getting the Connection data. (OPTIONAL)
+ * dejirenではCordovaをiOSとAndroidで利用している。
+ * iOSではcordova-plugin-network-informationを有効にするとオフライン起動がうまく機能しなくなる。
+ * Androidではcordova-plugin-network-informationを有効にするとネットワーク接続状態が正しく反映されるようになる。
+ * よってAndroidでのみcordova-plugin-network-informationを有効にする。
  */
-NetworkConnection.prototype.getInfo = function (successCallback, errorCallback) {
-    exec(successCallback, errorCallback, 'NetworkStatus', 'getConnectionInfo', []);
-};
+if (cordova.platformId === 'android') {
 
-var me = new NetworkConnection();
-var timerId = null;
-var timeout = 500;
+    // Link the onLine property with the Cordova-supplied network info.
+    // This works because we clobber the navigator object with our own
+    // object in bootstrap.js.
+    // Browser platform do not need to define this property, because
+    // it is already supported by modern browsers
+    if (cordova.platformId !== 'browser' && typeof navigator !== 'undefined') {
+        utils.defineGetter(navigator, 'onLine', function () {
+            return this.connection.type !== 'none';
+        });
+    }
 
-channel.createSticky('onCordovaConnectionReady');
-channel.waitForInitialization('onCordovaConnectionReady');
+    function NetworkConnection () {
+        this.type = 'unknown';
+    }
 
-channel.onCordovaReady.subscribe(function () {
-    me.getInfo(function (info) {
-        me.type = info;
-        if (info === 'none') {
-            // set a timer if still offline at the end of timer send the offline event
-            timerId = setTimeout(function () {
-                cordova.fireDocumentEvent('offline');
-                timerId = null;
-            }, timeout);
-        } else {
-            // If there is a current offline event pending clear it
-            if (timerId !== null) {
-                clearTimeout(timerId);
-                timerId = null;
+    /**
+     * Get connection info
+     *
+     * @param {Function} successCallback The function to call when the Connection data is available
+     * @param {Function} errorCallback The function to call when there is an error getting the Connection data. (OPTIONAL)
+     */
+    NetworkConnection.prototype.getInfo = function (successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'NetworkStatus', 'getConnectionInfo', []);
+    };
+
+    var me = new NetworkConnection();
+    var timerId = null;
+    var timeout = 500;
+
+    channel.createSticky('onCordovaConnectionReady');
+    channel.waitForInitialization('onCordovaConnectionReady');
+
+    channel.onCordovaReady.subscribe(function () {
+        me.getInfo(function (info) {
+            me.type = info;
+            if (info === 'none') {
+                // set a timer if still offline at the end of timer send the offline event
+                timerId = setTimeout(function () {
+                    cordova.fireDocumentEvent('offline');
+                    timerId = null;
+                }, timeout);
+            } else {
+                // If there is a current offline event pending clear it
+                if (timerId !== null) {
+                    clearTimeout(timerId);
+                    timerId = null;
+                }
+                cordova.fireDocumentEvent('online');
             }
-            cordova.fireDocumentEvent('online');
-        }
 
-        // should only fire this once
-        if (channel.onCordovaConnectionReady.state !== 2) {
-            channel.onCordovaConnectionReady.fire();
-        }
-    },
-    function (e) {
-        // If we can't get the network info we should still tell Cordova
-        // to fire the deviceready event.
-        if (channel.onCordovaConnectionReady.state !== 2) {
-            channel.onCordovaConnectionReady.fire();
-        }
-        console.log('Error initializing Network Connection: ' + e);
+            // should only fire this once
+            if (channel.onCordovaConnectionReady.state !== 2) {
+                channel.onCordovaConnectionReady.fire();
+            }
+        },
+        function (e) {
+            // If we can't get the network info we should still tell Cordova
+            // to fire the deviceready event.
+            if (channel.onCordovaConnectionReady.state !== 2) {
+                channel.onCordovaConnectionReady.fire();
+            }
+            console.log('Error initializing Network Connection: ' + e);
+        });
     });
-});
+
+}
 
 module.exports = me;
